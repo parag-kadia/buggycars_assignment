5c4ab37378288f5e7b513c312407a778014b7daf
parag-kadia <58125009+parag-kadia@users.noreply.github.com>
2023-04-18 11:09:43 +0530

    Add files via upload

---
+++ reports/cucumber-html-reports/timeline/report.js
@@ -0,0 +1,132 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([
+  {
+    "id": "cb516ebe-9090-45df-aa01-a453f39c032f",
+    "feature": "Buggy Cars Rating User Profile",
+    "scenario": "Update profile",
+    "start": 1617890489184,
+    "group": 18,
+    "content": "",
+    "tags": "@profile,@wip,",
+    "end": 1617890498105,
+    "className": "passed"
+  },
+  {
+    "id": "691586c3-b7ab-4953-8d35-260513cd7ab9",
+    "feature": "Vote for a Selected Car",
+    "scenario": "Vote for a car without a comment - \"Lamborghini\" \u0026 \"Diablo\"",
+    "start": 1617890542776,
+    "group": 20,
+    "content": "",
+    "tags": "@vote,",
+    "end": 1617890549233,
+    "className": "failed"
+  },
+  {
+    "id": "7c38ba17-f103-420b-bb29-e33e7ef53ba1",
+    "feature": "Vote for a Selected Car",
+    "scenario": "Vote for a car with a comment - \"Lamborghini\" \u0026 \"Diablo\"",
+    "start": 1617890489181,
+    "group": 20,
+    "content": "",
+    "tags": "@vote,",
+    "end": 1617890501237,
+    "className": "failed"
+  },
+  {
+    "id": "84d55162-9fd4-40ae-ae72-cd7956c3da51",
+    "feature": "Login to Buggy Cars Rating",
+    "scenario": "Login as a valid user",
+    "start": 1617890489179,
+    "group": 17,
+    "content": "",
+    "tags": "@login,",
+    "end": 1617890495090,
+    "className": "passed"
+  },
+  {
+    "id": "c45c4182-f679-4422-a532-834769efae1c",
+    "feature": "Register with Buggy Cars Rating",
+    "scenario": "Register with duplicate user information",
+    "start": 1617890495729,
+    "group": 19,
+    "content": "",
+    "tags": "@register,@negative,",
+    "end": 1617890500021,
+    "className": "passed"
+  },
+  {
+    "id": "26180109-9be1-4fb7-879c-87a617c1199e",
+    "feature": "Vote for a Selected Car",
+    "scenario": "Vote for a car without a comment - \"Alfa Romeo\" \u0026 \"Giulietta\"",
+    "start": 1617890521624,
+    "group": 20,
+    "content": "",
+    "tags": "@vote,",
+    "end": 1617890542766,
+    "className": "failed"
+  },
+  {
+    "id": "34745e81-0045-4468-b626-c5f8612c2389",
+    "feature": "Register with Buggy Cars Rating",
+    "scenario": "Register with valid user information",
+    "start": 1617890489183,
+    "group": 19,
+    "content": "",
+    "tags": "@register,",
+    "end": 1617890495708,
+    "className": "passed"
+  },
+  {
+    "id": "5621049f-c226-4419-b7b7-1d44ac22b05b",
+    "feature": "Login to Buggy Cars Rating",
+    "scenario": "Login as an invalid user",
+    "start": 1617890495131,
+    "group": 17,
+    "content": "",
+    "tags": "@login,@negative,",
+    "end": 1617890499151,
+    "className": "passed"
+  },
+  {
+    "id": "4f225f43-62ed-4f55-afe0-5b1e2c53d9d1",
+    "feature": "Buggy Cars Rating User Profile",
+    "scenario": "Update profile",
+    "start": 1617890498126,
+    "group": 18,
+    "content": "",
+    "tags": "@profile,@wip,",
+    "end": 1617890504208,
+    "className": "passed"
+  },
+  {
+    "id": "dae7336f-a109-4023-bbf5-1c3f50b8db66",
+    "feature": "Vote for a Selected Car",
+    "scenario": "Vote for a car with a comment - \"Lamborghini\" \u0026 \"Murciélago\"",
+    "start": 1617890501257,
+    "group": 20,
+    "content": "",
+    "tags": "@vote,",
+    "end": 1617890521614,
+    "className": "passed"
+  }
+]);
+CucumberHTML.timelineGroups.pushArray([
+  {
+    "id": 17,
+    "content": "Thread[pool-1-thread-1,5,main]"
+  },
+  {
+    "id": 18,
+    "content": "Thread[pool-1-thread-2,5,main]"
+  },
+  {
+    "id": 19,
+    "content": "Thread[pool-1-thread-3,5,main]"
+  },
+  {
+    "id": 20,
+    "content": "Thread[pool-1-thread-4,5,main]"
+  }
+]);
+});
